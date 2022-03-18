3cbd5ab22a7fa7c9487c8796a063a1ae70ff703c
Roeel <101860314+Roeel@users.noreply.github.com>
2022-03-18 15:35:37 +0800

    Update 58tc.js

--- 58tc.js
+++ 58tc.js
@@ -26,7 +26,7 @@ https://magicisland.58.com/web/sign/getIndexSignInInfo url script-request-header
 [MITM]
 hostname = magicisland.58.com
 */
-const $ = Env('58同城')
+const $ = new Env('58同城');
 const logDebug = 0
 
 const notifyFlag = 1; //0为关闭通知，1为打开通知,默认为1
