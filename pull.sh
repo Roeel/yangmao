470a97fee3ff4eb2c2cac0677afd56ccd8eaf0e3
Roeel <101860314+Roeel@users.noreply.github.com>
2022-03-19 13:16:46 +0800

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -4,3 +4,4 @@ ql repo https://gitee.com/soy-tool/app-script.git 'app_yhxq' #源火
 ql repo https://github.com/leafxcy/JavaScript.git 'ks.js|ksjsb|txstock' #快手，自选股
 ql repo https://github.com/yml2213/javascript.git 'sytt|lmz' #十堰，狸猫
 ql raw http://nm.nm6.ltd/scripts/kk.js #咔咔
+ql repo https://github.com/zyf1118/kevinxf.git 'sfsy' #顺丰
