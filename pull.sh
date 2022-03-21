af1270b1ffe6fbcdde6dce101f409173bf8bb85a
Roeel <101860314+Roeel@users.noreply.github.com>
2022-03-21 09:53:02 +0800

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #40 * * * * pull.sh, tag=拉取
 ql repo https://gitee.com/xiecoll/radish-script.git "lb_|Chinaunicom" #饿了么，美团，联通
-ql repo https://gitee.com/soy-tool/app-script.git 'app_yhxq' #源火
+ql repo https://gitee.com/soy-tool/app-script.git 'app_yhxq|wytk' #源火
 ql repo https://github.com/leafxcy/JavaScript.git 'ks.js|ksjsb|txstock' #快手，自选股
 ql repo https://github.com/yml2213/javascript.git 'sytt|lmz' #十堰，狸猫
 ql raw http://nm.nm6.ltd/scripts/kk.js #咔咔
