8b910a6eb9388e892f09e0176e4375e04728747e
chanho0 <53634547+chanho0@users.noreply.github.com>
2022-04-23 21:44:44 +0800

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -2,7 +2,7 @@
 ql repo https://gitee.com/xiecoll/radish-script.git "lb_|Chinaunicom" #饿了么，美团，联通
 ql repo https://gitee.com/soy-tool/app-script.git 'app_yhxq|wytk|app_dwsj|app_kyin|wx_yxxsh' #源火,闪电世界，快音
 #ql repo https://github.com/leafxcy/JavaScript.git 'ks.js|ksjsb|txstock' #快手，自选股
-ql repo https://github.com/yml2213/javascript.git 'sytt|lmz|xlh' 'invalid' #十堰，狸猫,骁龙会
+ql repo https://github.com/yml2213/javascript.git 'sytt|lmz|xlh|ydcd' 'invalid' #十堰，狸猫,骁龙会
 ql raw http://nm.nm6.ltd/scripts/kk.js #咔咔
 ql repo https://github.com/zyf1118/kevinxf.git 'sfsy' #顺丰
 ql raw https://raw.githubusercontent.com/FengYun27/YunFeng_Repo/main/other/gqsl.js #广汽三菱
